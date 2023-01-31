ec6ef23680094783461b5d58b28a2f350ece0014
shuygena <i.am.zuzya@gmail.com>
2023-01-31 22:27:17 +0300

    "add logger, set main menu, handlers"

--- bot.py
+++ bot.py
@@ -4,11 +4,34 @@ import logging
 from aiogram import Bot, Dispatcher
 from typing import Dict
 
-from config_data.config import Config, load_config, load_words
+from config_data.config import Config, load_config # i need it?
+from database.database import words, users_db, RUS, ENG, EASY, NORMAL, HARD
+from keyboards.main_menu import set_main_menu
+from handlers.user_handlers import register_user_handlers
+# from handlers.other_handlers import register_other_handlers
+
+logger = logging.getLogger(__name__)
+
+
+def register_all_handlers(dp: Dispatcher) -> None:
+    register_user_handlers(dp)
+    # register_other_handlers(dp)
 
 async def main():
+    logging.basicConfig(
+        level = logging.INFO,
+        format = u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
+    logger.info('Starting bot')
     config: Config = load_config()
-    words: Dict = load_words()
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher(bot)
+    await set_main_menu(dp)
+    register_all_handlers(dp)
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.close()
 
 if __name__ == '__main__':
     try:
