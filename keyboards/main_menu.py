b0a2706bc6d8c685a827c039d04276984b8c3bb0
shuygena <i.am.zuzya@gmail.com>
2023-02-02 21:57:24 +0300

    add commands in main menu

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -3,15 +3,16 @@ from aiogram import Dispatcher, types
 
 async def set_main_menu(dp: Dispatcher):
     main_menu_commands = [
+        types.BotCommand(command='/start', description='Запустить бота'),
         types.BotCommand(command='/game', description='Начать игру'),
         types.BotCommand(command='/rules', description='Правила игры'),
-        types.BotCommand(command='/help', description='help'),
+        types.BotCommand(command='/help', description='Помощь'),
         types.BotCommand(command='/info', description='Информация о текущей игре'),
         types.BotCommand(command='/set_language', description='Изменить язык'),
         types.BotCommand(command='/set_level', description='Изменить уровень сложности'),
-        types.BotCommand(command='/set_points', description='Изменить условие победы'),
+        types.BotCommand(command='/set_score_to_win', description='Изменить условие победы'),
         types.BotCommand(command='/set_time', description='Изменить длительность раунда'),
-        types.BotCommand(command='/set_pass_tax', description='Рассчёт пропущенных строк')
+        types.BotCommand(command='/set_pass_tax', description='Рассчёт пропущенных слов')
         # Последнее слово объяснять сколько хочешь
         # Изменить название команды
         # Остановить игру
