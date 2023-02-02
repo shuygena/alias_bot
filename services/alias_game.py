47f01aad0666d2781ddb0ec83eb7ecc95bfcd934
shuygena <i.am.zuzya@gmail.com>
2023-02-02 21:46:05 +0300

    add passed words control in genetate_new_word

--- services/alias_game.py
+++ services/alias_game.py
@@ -1,8 +1,21 @@
-from random import randint, seed
-from typing import Dict
-from database.database import words
+from random import choice, seed
+from typing import Set
+from database.database import users_db
 
-def generate_new_word(language: str, level: str) -> Dict:
+def generate_new_word(user_id) -> str:
     seed()
-    number = randint(1, len(words[language][level]))
-    return number, words[language][level][number]
+    game_words: Set = users_db[user_id].get_game_words()
+    if len(game_words) == 0: # что если и len(passed_words) == 
+        passed_words = users_db[user_id].get_passed_words()
+        if len(passed_words) > 0:
+            users_db[user_id].set_game_words(passed_words)
+        else:
+            users_db[user_id].reset_game_words()
+        game_words = users_db[user_id].get_game_words()
+        users_db[user_id].reset_passed_words()
+    word: str = choice(game_words)
+    users_db[user_id].remove_game_words(word)
+    return word
+
+def get_winner():
+    pass
