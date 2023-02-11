c6248ac8b3d5735f52ca0cf11074898ae4679599
shuygena <i.am.zuzya@gmail.com>
2023-02-11 03:20:06 +0300

    add set_time

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,7 @@ from aiogram import Dispatcher
 from aiogram.types import CallbackQuery, Message
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher.filters import Text
+
 from database.database import Game, users_db
 from lexicon.lexicon import LEXICON
 from keyboards.keyboards import get_new_game_kb, get_ready_kb, get_guess_pass_kb
@@ -14,6 +14,8 @@ class FSMFillForm(StatesGroup):
     set_language = State()
     set_level = State()
     set_pass_tax = State()
+    set_time = State()
+    set_score_to_win = State()
 
 
 async def process_start_command(message: Message):
@@ -45,6 +47,25 @@ async def process_game_press(callback: CallbackQuery):
     await FSMFillForm.fill_teams_names.set()
 
 
+async def warning_not_names(message: Message):
+    await message.answer(text=LEXICON['not_names'])
+
+
+async def game_start(message: Message, state: FSMContext): # проверить на сброс параметров
+    await state.finish()
+    user_id: int = message.from_user.id
+    names_list: List = message.text.split('\n')
+    users_db[user_id].init_teams(names_list)
+    users_db[user_id].set_game_status(True)
+    users_db[user_id].move_current_team()
+    users_db[user_id].reset_current_word()
+    users_db[user_id].set_time_is_over(False)
+    users_db[user_id].reset_game_words()
+    team_name = users_db[user_id].get_teams()[1]['name']
+    text = f'Очередь команды «{team_name}»\n\nГотовы?'
+    await message.answer(text = text, reply_markup = get_ready_kb())
+
+
 async def process_set_language_command(message: Message):
     await message.answer(LEXICON[message.text],
                         reply_markup = get_language_kb())
@@ -57,19 +78,19 @@ async def set_language(callback: CallbackQuery, state: FSMContext):
     language: str = callback.data
     users_db[user_id].set_language(language)
     await callback.answer()
-    await callback.message.edit_text(text = callback.message.text  +
-                                    '\n\n⚙️ <b>Настройки изменены!</b>')
+    await callback.message.edit_text(text = LEXICON['/set_language']  +
+                                    '\n\n⚙️ <b>Настройки сохранены!</b>')
 
 
 async def warning_not_language(message: Message):
-    await message.answer(text='Пожалуйста, пользуйcя кнопками '
-                              'при выборе языка')
+    await message.answer(LEXICON['use_buttons'])
 
 
 async def process_set_level_command(message: Message):
     await message.answer(LEXICON[message.text],
                         reply_markup = get_level_kb())
-    await FSMFillForm.set_level.set() 
+    await FSMFillForm.set_level.set()
+
 
 async def set_level(callback: CallbackQuery, state: FSMContext):
     await state.finish()
@@ -78,12 +99,11 @@ async def set_level(callback: CallbackQuery, state: FSMContext):
     users_db[user_id].set_level(level)
     await callback.answer()
     await callback.message.edit_text(text = callback.message.text +
-                                    '\n\n⚙️ <b>Настройки изменены!</b>')
+                                    '\n\n⚙️ <b>Настройки сохранены!</b>')
 
 
 async def warning_not_level(message: Message):
-    await message.answer(text='Пожалуйста, пользуйcя кнопками '
-                              'при выборе уровня сложности')
+    await message.answer(LEXICON['use_buttons'])
 
 
 async def process_set_pass_tax_command(message: Message):
@@ -102,31 +122,28 @@ async def set_pass_tax(callback: CallbackQuery, state: FSMContext):
         users_db[user_id].set_pass_tax(False)
     await callback.answer()
     await callback.message.edit_text(text = callback.message.text +
-                                    '\n\n⚙️ <b>Настройки изменены!</b>')
+                                    '\n\n⚙️ <b>Настройки сохранены!</b>')
 
 
 async def warning_not_tax(message: Message):
-    await message.answer(text='Пожалуйста, пользуйcя кнопками')
+    await message.answer(LEXICON['use_buttons'])
 
 
-async def game_start(message: Message, state: FSMContext): # проверить на сброс параметров
+async def process_set_time_command(message: Message):
+    await message.answer(LEXICON[message.text])
+    await FSMFillForm.set_time.set() 
+
+
+async def set_time(message: Message, state: FSMContext):
     await state.finish()
     user_id: int = message.from_user.id
-    names_list: List = message.text.split('\n')
-    users_db[user_id].init_teams(names_list)
-    users_db[user_id].set_game_status(True)
-    users_db[user_id].move_current_team()
-    users_db[user_id].reset_current_word()
-    users_db[user_id].set_time_is_over(False)
-    users_db[user_id].reset_game_words()
-    team_name = users_db[user_id].get_teams()[1]['name']
-    text = f'Очередь команды «{team_name}»\n\nГотовы?'
-    
-    await message.answer(text = text, reply_markup = get_ready_kb())
+    time: str = int(message.text)
+    users_db[user_id].set_time(time)
+    await message.answer('⚙️ <b>Настройки сохранены!</b>')
 
 
-async def warning_not_names(message: Message):
-    await message.answer(text=LEXICON['not_names'])
+async def warning_not_time(message: Message):
+    await message.answer(text='Введи число от 10 до 300 ❗️')
 
 
 def register_user_handlers(dp: Dispatcher):
@@ -141,15 +158,13 @@ def register_user_handlers(dp: Dispatcher):
                                 commands=['set_level'])
     dp.register_message_handler(process_set_pass_tax_command,
                                 commands=['set_pass_tax'])
-    
     # dp.register_message_handler(process_set_score_to_win_command,
     #                             commands=['set_score_to_win'])
-    # dp.register_message_handler(process_set_time_command,
-    #                             commands=['set_time'])
+    dp.register_message_handler(process_set_time_command,
+                                commands=['set_time'])
     # dp.register_message_handler(process_set_reset_command,
-    #                             commands=['set_reset'])
-    # dp.register_message_handler(set_language,
-    #                             Text(equals=['RUS', 'ENG', 'TAU']))
+    #                             commands=['reset'])
+
     dp.register_callback_query_handler(set_language,
                                    text=['rus', 'eng', 'tau'],
                                    state=FSMFillForm.set_language)
@@ -159,13 +174,15 @@ def register_user_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(set_pass_tax,
                                 text=['True', 'False'],
                                 state=FSMFillForm.set_pass_tax)
-
     dp.register_callback_query_handler(process_game_press,
                                     text='new_game_button_pressed')
 
     dp.register_message_handler(game_start,
-                                lambda x: len(x.text) > 0 and len(x.text.split('\n')) < 5,
-                                state=FSMFillForm.fill_teams_names)
+                            lambda x: len(x.text) > 0 and len(x.text.split('\n')) < 5,
+                            state=FSMFillForm.fill_teams_names)
+    dp.register_message_handler(set_time,
+                lambda x: x.text.isdigit() and 10 <= int(x.text) <= 300,
+                                state=FSMFillForm.set_time)
 
     dp.register_message_handler(warning_not_names,
                             content_types='any',
@@ -179,3 +196,6 @@ def register_user_handlers(dp: Dispatcher):
     dp.register_message_handler(warning_not_tax,
                             content_types='any',
                             state=FSMFillForm.set_pass_tax)
+    dp.register_message_handler(warning_not_time,
+                            content_types='any',
+                            state=FSMFillForm.set_time)
